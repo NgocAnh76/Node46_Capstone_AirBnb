59728ba764ec185ab637d498832fc84e9be5c96b
NgocAnh76 <ngocanh76.dev@gmail.com>
2025-05-21 00:14:06 +0700

    update

--- src/common/constant/app.constant.ts
+++ src/common/constant/app.constant.ts
@@ -1,4 +1,4 @@
-import 'dotenv/config'
+import 'dotenv/config';
 
 export const DATABASE_URL = process.env.DATABASE_URL;
 
@@ -8,12 +8,13 @@ export const ACCESS_TOKEN_EXPIRED = process.env.ACCESS_TOKEN_EXPIRED;
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 export const REFRESH_TOKEN_EXPIRED = process.env.REFRESH_TOKEN_EXPIRED;
 
-export const REGEX_EMAIL = /(?=^[a-z0-9.]+@[a-z0-9.-]+\.[a-zA-Z]{2,6}$)(?=^.{1,40}$)/i;
+export const REGEX_EMAIL =
+  /(?=^[a-z0-9.]+@[a-z0-9.-]+\.[a-zA-Z]{2,6}$)(?=^.{1,40}$)/i;
 
 console.log({
-   DATABASE_URL,
-   ACCESS_TOKEN_SECRET,
-   ACCESS_TOKEN_EXPIRED,
-   REFRESH_TOKEN_SECRET,
-   REFRESH_TOKEN_EXPIRED
+  DATABASE_URL,
+  ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRED,
+  REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRED,
 });
